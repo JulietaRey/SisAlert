4318d22da13e471ab8f8396d64bc856f5ef942c9
julieta <julieta@MacBook-Pro-de-julieta.local>
2018-06-06 23:00:32 -0300

    prueba de ruteo y composicion en escena

---
+++ sisalert/src/Components/Buttons/NuevaAlerta.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NuevaAlerta extends React.Component{
+    render(){
+        return (
+            <button className="primaryButton">
+                Nueva alerta
+            </button>
+        );
+    }
+}
+
+export default NuevaAlerta;
